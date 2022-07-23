2b7d823bf4a5bdfb25d4c2e86b940aa5ffa5fbcc
chiachun wu <a0283137@gmail.com>
2022-07-23 09:58:52 +0800

    遇到手機板被吃版的狀況  修復中

--- js/fadeslide.js
+++ js/fadeslide.js
@@ -1,14 +1,4 @@
-        //控制內文滑進來
-        let about = document.getElementById("about");
-        let mypic = document.getElementById("mypic");
-        let profile = document.getElementById("profile");
-
-
-
-
-
-
-
+        // 控制內文滑進來
 
         window.addEventListener('scroll', function () {
             var value = window.scrollY;
@@ -18,16 +8,20 @@
                     top: '0px',
                     opacity: "1",
                     height: '200px',
-                    width: '200px'
+                    width: '200px',
+                    
                   },1000);
+
+
                 $("#profile").animate({
                     left: '0px',
                     top: '0px',
                     opacity: "1",
                     height: '200px',
-                    width: '200px'
+                    width: '200px',
+                    
                   },1000);
             }
 
-            // text.style.fontSize = '10em' / value + 'px';
+            
         })
